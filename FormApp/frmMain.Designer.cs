dc7dd60e0aeacc5869984890f1ea2123dd6346b1
Derek Appleby <theseaside25@tiscali.co.uk>
2016-11-03 09:57:45 +0000

    Add project files.

---
+++ FormApp/frmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace FormApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdGoAway = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
+            // 
+            // cmdGoAway
+            // 
+            this.cmdGoAway.Location = new System.Drawing.Point(137, 184);
+            this.cmdGoAway.Name = "cmdGoAway";
+            this.cmdGoAway.Size = new System.Drawing.Size(75, 23);
+            this.cmdGoAway.TabIndex = 1;
+            this.cmdGoAway.Text = "Go Away";
+            this.cmdGoAway.UseVisualStyleBackColor = true;
+            this.cmdGoAway.Click += new System.EventHandler(this.cmdGoAway_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 261);
+            this.Controls.Add(this.cmdGoAway);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdGoAway;
+    }
+}
+
---
+++ FormApp/frmMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApp
+{
+    public partial class frmMain : Form
+    {
+        // ========
+        // LIFETIME
+        // ========
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // ===
+        // DOs
+        // ===
+
+        // -------------------------------------------------------------------
+        //
+        private void DoGoAway()
+        {
+            this.Close();
+        }
+
+        // ================
+        // COMMAND HANDLERS
+        // ================
+
+        private void cmdGoAway_Click(object sender, EventArgs e) { this.DoGoAway(); }
+    }
+}
